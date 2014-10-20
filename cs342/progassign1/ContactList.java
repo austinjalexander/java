489907dfa51af7a4e688ac5b112adaa8ceb3efa0
Austin J. Alexander <austinjalexander@gmail.com>
2014-10-19 23:06:42 -0400

    frozen on multiple contacts list

--- cs342/progassign1/ContactList.java
+++ cs342/progassign1/ContactList.java
@@ -21,195 +21,122 @@ public class ContactList {
   // PUBLIC METHODS
   public void addNewContact(String full_name, String email, String phone_number) {
 
+    // capitalize name for simpler string/character comparison
+    full_name = full_name.toUpperCase();
+
+    // list_length == 0
     //*** if the list is empty ***//
     if (this.first_contact == null) {
       this.first_contact = new Contact(full_name, email, phone_number);
     }
 
+    // list_length == 1
     //*** if the list has one contact ****//
     else if (this.first_contact.isNextContactNull() == true) {
 
       // make full_name string comparison
       int comparison = this.first_contact.getFullName().compareTo(full_name);
 
-      // if first_contact.full_name and new_contact.full_name
-      // are lexicographically equivalent (comparison = 0), 
-      // order doesn't matter,
-      // set new_contact as last contact in list
-      if (comparison == 0) {
+      // first(A), new(A) || first(A), new(B)
+      /* if first_contact.full_name and new_contact.full_name
+       * are lexicographically equivalent (comparison = 0),
+       * OR if first_contact.full_name lexicographically precedes
+       * new_contact.full_name (comparison < 0) 
+       * set new_contact as last contact in list */
+      if (comparison == 0 || comparison < 0) {
         this.first_contact.setNextContact(new Contact(full_name, email, phone_number));
       }
 
-      // if first_contact.full_name is lexicographically before
-      // new_contact.full_name (comparison < 0),
-      // set new_contact as first contact in list  
-      else if (comparison < 0) {
-        Contact new_contact = new Contact(full_name, email, phone_number, this.first_contact));
+      // new(A), first(B)
+      /* else if first_contact.full_name lexicographically follows
+       * new_contact.full_name (comparison > 0),
+       * set new_contact as first contact in list */
+      else if (comparison > 0) {
+        Contact new_contact = new Contact(full_name, email, phone_number, this.first_contact);
         this.first_contact = new_contact;
       } 
-
-      // if first_contact.full_name is lexicographically after
-      // new_contact.full_name (comparison > 0),
-      // set new_contact as last contact in list  
-      else if (comparison > 0) {
-        this.first_contact.setNextContact(new Contact(full_name, email, phone_number));
-      }
     }
 
+    // list_length > 1
     //*** if the list has more than one contact ****//
     else if (this.first_contact.isNextContactNull() == false) {
 
       // make full_name string comparison
       int comparison = this.first_contact.getFullName().compareTo(full_name);
 
-      // if first_contact.full_name and new_contact.full_name
-      // are lexicographically equivalent (comparison = 0), 
-      // order doesn't matter,
-      // set new_contact as second contact in list,
-      // move original second contact to third contact
-      if (comparison == 0) {
-        Contact second_contact = this.first_contact.getNextContact();
-
-        this.first_contact.setNextContact(new Contact(full_name, email, 
-                                                      phone_number, second_contact));
-      }
-
-      // if first_contact.full_name is lexicographically before
-      // new_contact.full_name (comparison < 0),
-      // set new_contact as first contact in list  
-      else if (comparison < 0) {
-        Contact new_contact = new Contact(full_name, email, phone_number, this.first_contact));
+      // new(A), first(B)
+      /* if first_contact.full_name lexicographically follows
+       * new_contact.full_name (comparison > 0),
+       * set new_contact as first contact in list */
+      if (comparison > 0) {
+        Contact new_contact = new Contact(full_name, email, phone_number, this.first_contact);
         this.first_contact = new_contact;
       } 
 
-      // if first_contact.full_name is lexicographically after
-      // new_contact.full_name (comparison > 0),
-      // loop through to find appropriate location 
-      else if (comparison > 0) {
+      // first(A), new(A) 
+      // OR
+      // first(A), .?., new(B), next(C)
+      /* else first_contact.full_name and new_contact.full_name
+       * are lexicographically equivalent (comparison = 0),
+       * OR first_contact.full_name lexicographically precedes
+       * new_contact.full_name (comparison < 0),
+       * find the proper lexicographic location for new_contact 
+       * by looping through the list */
+      else if (comparison == 0 || comparison < 0) {
+
         Contact current_contact = this.first_contact;
         Contact next_contact = this.first_contact.getNextContact();
 
-        Boolean continue_loop = true;
-        while (continue_loop == true) {
+        Boolean found_location = false;
+        while (found_location == false) {
 
           // make full_name string comparisons
-          int current_comparison = this.first_contact.getFullName().compareTo(full_name);
-          int next_comparison = this.first_contact.getNextContact().getFullName().compareTo(full_name);
+          int current_contact_comparison = this.first_contact.getFullName().compareTo(full_name);
+          int next_contact_comparison = this.first_contact.getNextContact().getFullName().compareTo(full_name);
 
-          // if current_contact.full_name and new_contact.full_name
-          // are lexicographically equivalent (comparison = 0), 
-          // order doesn't matter,
-          // set new_contact as next contact in list,
-          // move original next_contact to new next contact position,
-          // exit loop
-          if (current_comparison == 0) {
-            this.first_contact.setNextContact(new Contact(full_name, email, 
+          // current(C), new(C), next(F)
+          // OR
+          // current(C), new(D), next(F)
+          /* if current_contact.full_name and new_contact.full_name
+           * are lexicographically equivalent (current_contact_comparison = 0),
+           * AND next_contact.full_name lexicographically follows
+           * new_contact.full_name (next_contact_comparison > 0),
+           * OR if current_contact.full_name lexicographically precedes
+           * new_contact.full_name (current_contact_comparison < 0) 
+           * AND next_contact.full_name lexicographically follows
+           * new_contact.full_name (next_contact_comparison > 0), 
+           * set new_contact as the contact in between 
+           * current_contact and next_contact */
+          if ((current_contact_comparison == 0 && next_contact_comparison > 0) || 
+               (current_contact_comparison < 0 && next_contact_comparison > 0)) {
+
+            current_contact.setNextContact(new Contact(full_name, email, 
                                                           phone_number, next_contact));
-            continue_loop = false;
+            found_location = true;
           }
 
-          // if current_contact.full_name is lexicographically before
-          // new_contact.full_name (comparison < 0),
-          // set new_contact as first contact in list  
-          else if (comparison < 0) {
-            Contact new_contact = new Contact(full_name, email, phone_number, this.first_contact));
-            this.first_contact = new_contact;
-          } 
+          // current(C), new(C), next(C)
+          // OR
+          // current(C), new(D), next(D)
+          /* if current_contact.full_name and new_contact.full_name
+           * are lexicographically equivalent (current_contact_comparison = 0),
+           * AND next_contact.full_name and new_contact.full_name
+           * are lexicographically equivalent (current_contact_comparison = 0),
+           * OR if current_contact.full_name lexicographically precedes
+           * new_contact.full_name (current_contact_comparison < 0) 
+           * AND next_contact.full_name and new_contact.full_name
+           * are lexicographically equivalent (current_contact_comparison = 0),
+           * iterate through the loop */
+          else if ((current_contact_comparison == 0 && next_contact_comparison == 0) || 
+               (current_contact_comparison < 0 && next_contact_comparison == 0)) {
 
-        }
-      }
-    } 
-
-
-
-
-
-    
-,
-    // OR
-    // if first_contact.full_name is lexicographically 
-    // after new_contact.full_name (comparison > 0)
-    else if (comparison == 0) {
-      
-
-    }
-
-/*
-
-
-      // if first_contact.full_name is lexicographically 
-      // equivalent to new_contact.full_name (comparison = 0),
-      // the order doesn't matter, set new_contact to first_contact.next_contact;
-      // if first_contact.full_name is lexicographically 
-      // before new_contact.full_name (comparison < 0), 
-      // set new_contact to first_contact.next_contact
-      if (comparison <= 0) {
-        this.first_contact.setNextContact(new Contact(full_name, email, phone_number));
-      }
-      // if first_contact.full_name is lexicographically 
-      // after new_contact.full_name (comparison > 0)
-      else if (comparison > 0) {
-        this.first_contact.setNextContact(null);
-        Contact new_contact = new Contact(full_name, email, phone_number, this.first_contact);
-        this.first_contact = new_contact;
-      }
-    }
-    // if the list has more than one contact
-    else {
-      Contact current_contact = this.first_contact;
-      Contact next_contact = this.first_contact.getNextContact();
-
-      Boolean continue_loop = true;
-      while (continue_loop == true) {
-
-        int current_contact_comparison = current_contact.getFullName().compareTo(full_name);
-        int next_contact_comparison = next_contact.getFullName().compareTo(full_name);
-
-        // if current_contact.full_name is lexicographically 
-        // equivalent to new_contact.full_name (current_contact_comparison = 0),
-        // the order doesn't matter, set new_contact to current_contact.next_contact,
-        // setting new_contact.next_contact to the original current_contact.next_contact
-        if (current_contact_comparison == 0) {
-          current_contact.setNextContact(new Contact(full_name, email, 
-                                                     phone_number, next_contact));
-        }
-        // if current_contact.full_name is lexicographically 
-        // before new_contact.full_name (current_contact_comparison < 0)
-        else if (current_contact_comparison < 0) {
-          // if current_contact is the end of the list,
-          // set next_contact to new_contact,
-          // then exit loop
-          if (current_contact.isNextContactNull() == true) {
-            current_contact.setNextContact(new Contact(full_name, email, 
-                                                     phone_number));
-            continue_loop = false;
+            found_location = false;
           }
-          // if next_contact.full_name is also lexicographically 
-          // before new_contact.full_name (next_contact_comparison < 0),
-          // set current_contact to next_contact,
-          // set a new next_contact,
-          // and go back through loop
-          else if (next_contact_comparison < 0) {
-            current_contact = current_contact.getNextContact();
-            next_contact = current_contact.getNextContact();
-          }
-          // if next_contact.full_name is lexicographically 
-          // after new_contact.full_name (comparison > 0),
-          // then new_contact has found its home,
-          // then exit loop 
-          else if (next_contact > 0) {
-            current_contact.setNextContact(new Contact(full_name, email, 
-                                                     phone_number, next_contact));
-            continue_loop = false;
-          }
-        }
-        // if current_contact.full_name is lexicographically 
-        // before new_contact.full_name (current_contact_comparison < 0)
-        else if ()
-      }
-    }
-    */
-  }
+
+        } // end: while (found_location == false)
+      } // end: else if (comparison < 0)
+    } // end: else if (this.first_contact.isNextContactNull() == false) 
+  } // end: public void addNewContact(String full_name, String email, String phone_number)
 
   public void displayList() {
     if (this.first_contact == null) {
--- cs342/progassign1/MyContacts.java
+++ cs342/progassign1/MyContacts.java
@@ -13,16 +13,43 @@ public class MyContacts {
 
     contact_list.displayList();
 
-    // Add new contact
-    //contact_list.addNewContact("Sara Pethe", "sarapethe@bu.edu", "617-353-0092");
+    // Add new, first contact
+    // list_length == 0
+    contact_list.addNewContact("Sara Pethe", "sarapethe@bu.edu", "617-353-0092");
+
+    /*** list_length == 1 ***/
+    // first(A), new(A)
+    //contact_list.addNewContact("Sara Pethe", "sarap@bu.edu", "617-358-1048");  
+
+    // new(A), first(B)
     //contact_list.addNewContact("Nate Goldman", "nateg@bu.edu", "617-353-2811");
 
-    //contact_list.displayList();
+    // first(A), new(B)
+    //contact_list.addNewContact("Cali Stephens", "cali@bu.edu", "617-353-0092");  
 
-    // Add new contact
-    contact_list.addNewContact("Nate Goldman", "nateg@bu.edu", "617-353-2811");
-    contact_list.addNewContact("Sara Pethe", "sarap@bu.edu", "617-353-1048");
-    contact_list.addNewContact("Cali Stephens", "cali@bu.edu", "617-353-0092");
+
+    contact_list.addNewContact("Austin Alexander", "austin@bu.edu", "617-353-3739");
+    /*** list_length > 1 ***/
+    // first(A), new(A)
+    //contact_list.addNewContact("Austin Alexander", "austinjalexander@bu.edu", "617-353-2008");
+
+    // new(A), first(B)
+    contact_list.addNewContact("Asher Alexander", "asher@bu.edu", "617-202-2008");
+
+
+    contact_list.addNewContact("Asher Alexander", "asher@gmail.com", "617-333-2008"); 
+    // first(A), .?., new(B), next(C)
+    
+      // current(C), new(C), next(F)
+      contact_list.addNewContact("Cali Stephens", "cali@bu.edu", "617-353-0092");
+      // OR
+      // current(C), new(D), next(F)
+      // current(C), new(C), next(C)
+      //contact_list.addNewContact("Asher Alexander", "asher@mit.edu", "617-555-2008");
+      // OR
+      // current(C), new(D), next(D)
+
+
 
     contact_list.displayList();
 
